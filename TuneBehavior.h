19ebc51dd01267630cba9aa094f17fc1246adafc
Corey <cmattis@comcast.net>
2014-08-31 16:38:05 -0700

    Create TuneBehavior.h

---
+++ TuneBehavior.h
@@ -0,0 +1,14 @@
+#ifndef	_TUNE_BEHAVIOR_
+#define _TUNE_BEHAVIOR_
+
+
+class TuneBehavior {
+
+	public: virtual ~TuneBehavior() = 0 {
+	};
+	public: virtual void Tune () const = 0;
+};
+
+
+
+#endif
