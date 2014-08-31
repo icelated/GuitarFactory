a3937267c8a09b019f46532bde0853502103ae67
Corey <cmattis@comcast.net>
2014-08-31 16:31:23 -0700

    Create Headers.h

---
+++ Headers.h
@@ -0,0 +1,20 @@
+
+#define _CRT_SECURE_NO_DEPRECATE 1
+
+#include <assert.h>		// for: assert
+#include <algorithm>	// for: sort
+#include <ctime>		// for: tm structure
+#include <memory>		// for: auto_ptr
+#include <map>			// for: map
+#include <iostream>		// for: cout, cin
+#include <list>			// for: list
+#include <sstream>		// for: stringstream
+#include <string>		// for: string
+#include <vector>		// for: vector
+#include <fstream>		// for: ifstream
+
+
+
+
+
+#endif
